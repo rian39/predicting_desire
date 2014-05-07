8144d383a0afcaccd1cf61583f560e4a6b16398d
abm <a.mackenzie@lancaster.ac.uk>
2014-05-07 13:42:56 +0200

    tidying up and writing tolist

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-pandoc --bibliography=/home/mackenza/Documents/notes/data_forms_thought.bib 
---bibliography=/home/mackenza/Documents/notes/machine_learning.bib
- predicting_desire.md -o predicting_desire.pdf & evince predicting_desire.pdf
+pandoc  --csl = harvard-sage.csl --bibliography=/home/mackenza/Documents/ref_bibs/data_forms_thought.bib --bibliography=/home/mackenza/Documents/ref_bibs/R.bib  --bibliography=/home/mackenza/Documents/ref_bibs/machine_learning.bib predicting_desire.md -o predicting_desire.pdf & evince predicting_desire.pdf
