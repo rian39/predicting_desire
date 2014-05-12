25169d356bd0ef53320f21585d58646f8e2dd7a7
abm <a.mackenzie@lancaster.ac.uk>
2014-05-12 09:12:42 +0200

    almost ready to submit

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-pandoc  --csl = harvard-sage.csl --bibliography=/home/mackenza/Documents/ref_bibs/data_forms_thought.bib --bibliography=/home/mackenza/Documents/ref_bibs/R.bib  --bibliography=/home/mackenza/Documents/ref_bibs/machine_learning.bib predicting_desire.md -o predicting_desire.pdf & evince predicting_desire.pdf
+pandoc --csl=harvard-sage.csl --bibliography=/home/mackenza/Documents/ref_bibs/data_forms_thought.bib --bibliography=/home/mackenza/Documents/ref_bibs/R.bib  --bibliography=/home/mackenza/Documents/ref_bibs/machine_learning.bib predicting_desire.md -o predicting_desire.pdf  
+
+pandoc  --csl=harvard-sage.csl  --bibliography=/home/mackenza/Documents/ref_bibs/data_forms_thought.bib --bibliography=/home/mackenza/Documents/ref_bibs/R.bib  --bibliography=/home/mackenza/Documents/ref_bibs/machine_learning.bib predicting_desire.md -o predicting_desire.odt 
+
+libreoffice predicting_desire.odt &
+evince predicting_desire.pdf &
--- plan.md
+++ plan.md
@@ -1,4 +1,13 @@
+##TODO
 
+1. check headings and subheadings map -- DONE
+2. add a pointcloud for vectorisation
+3. decide on the title - DONE
+4. add a bit more on aggregates that are not people
+5. add comment on why retail analytics has been so successful
+6. discuss what happens as prediction is generalized
+
+## Plan
 1. vast spaces in cultural studies: the problem of studying these;  material actions; plots; problematization around accountability for going public; interventions in processes of individuation (human, non-human, and human-nonhuman) via new forms of aggregate;
 2. fetishisation of algorithms -- whose fetish; 2 senses of these: if  algorithms become the social connective tissue, then they tend to become the object of attention; they take on a life of their own (cf 'her')
 4. the practices of generalization -- how the techniques generalize;
